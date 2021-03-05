e84a867dd37df601ff8daa422eb13ecced2bc1ff
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-03-05 16:58:22 +0900

    Initial commit

---
+++ src/main/java/com/example/portalserviceksb/PortalserviceksbApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalserviceksb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalserviceksbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalserviceksbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portalserviceksb/PortalserviceksbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portalserviceksb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalserviceksbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
