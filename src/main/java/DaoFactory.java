c8633dc8b32206e07eed887c562b34cdc453ad68
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-04-09 18:42:46 +0900

    JdbcTemplate

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,6 +1,7 @@
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -26,12 +27,12 @@ public class DaoFactory {
 //    url = "jdbc:mysql://localhost/jeju?characterEncoding=utf-8&serverTimezone=UTC";
     @Bean
     public UserDao userDao() throws ClassNotFoundException {
-        return new UserDao(jdbcContext());
+        return new UserDao(jdbcTemplate());
     }
 
     @Bean
-    public JdbcContext jdbcContext() throws ClassNotFoundException {
-        return new JdbcContext(dataSource());
+    public JdbcTemplate jdbcTemplate() throws ClassNotFoundException {
+        return new JdbcTemplate(dataSource());
     }
 
     @Bean
--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -1,35 +1,58 @@
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.*;
 
 public class UserDao {
 
-    private final JdbcContext jdbcContext;
+    //private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public User findById(Integer id) throws SQLException {
         String sql = "select * from  userinfo where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.findById(id, sql, params);
+        return jdbcTemplate.query(sql, rs -> {
+            User user = null;
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+            return user;
+        },id);
     }
 
     public void insert(User user) throws SQLException {
         String sql = "insert into userinfo(name,password) value(?,?)";
         Object[] params = new Object[]{user.getName(),user.getPassword()};
-        jdbcContext.insert(user, sql, params, this);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con ->{
+            //db마다 auto increament방법이 다르기 때문에 직접 만들어 야 한줘다.
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        user.setId(keyHolder.getKey().intValue());
     }
 
     public void update(User user) throws SQLException {
         String sql = "update userinfo set name=?,password=? where id=?";
         Object[] params = new Object[]{user.getName(),user.getPassword(),user.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Integer id) throws SQLException {
         String sql = "delete from userinfo where id=?";
         Object[] params = new Object[]{id};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
 }
