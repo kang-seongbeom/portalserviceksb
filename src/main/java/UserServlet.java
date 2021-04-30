a838f4ee3558f869e9db0e057c365e5f256a2207
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-04-30 18:29:29 +0900

    service

--- src/main/java/UserServlet.java
+++ src/main/java/UserServlet.java
@@ -15,6 +15,14 @@ public class UserServlet extends GenericServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("***************service***************");
-
+        StringBuffer response = new StringBuffer();
+        response.append("<html>");
+        response.append("<Body>");
+        response.append("<h1>");
+        response.append("hello world");
+        response.append("</h1>");
+        response.append("</Body>");
+        response.append("</html>");
+        res.getWriter().println(response.toString());
     }
 }
