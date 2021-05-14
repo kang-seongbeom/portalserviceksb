147a0623623d44582ed5b102d731c42fa768008b
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-14 18:47:59 +0900

    servlet linstener

---
+++ src/main/java/UserContextLinstener.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class UserContextLinstener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.printf("*************** context init ***************");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.printf("*************** context destroy ***************");
+    }
+}
