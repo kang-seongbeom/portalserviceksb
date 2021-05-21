cfa2a98d03fa64f13b2b1a2ff134eafd25e21e5c
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-21 22:54:55 +0900

    mvc3

--- src/main/java/kr/ac/jejunu/SimpleController.java
+++ src/main/java/kr/ac/jejunu/SimpleController.java
@@ -15,7 +15,7 @@ public class SimpleController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         User user = userDao.findById(Integer.valueOf(request.getParameter("id")));
-        ModelAndView modelAndView = new ModelAndView(); //SimpleUrlHandlerMapping의경우 파라미터에 bean을 넣어줘야함(잘안씀)
+        ModelAndView modelAndView = new ModelAndView("user"); //SimpleUrlHandlerMapping의경우 파라미터에 bean을 넣어줘야함(잘안씀)
         modelAndView.addObject("user", user);
         return modelAndView;
     }
