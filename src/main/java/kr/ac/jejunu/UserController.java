bde8265310455d0d22a6115a5d5ba201273fcee5
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-28 18:51:20 +0900

    get,post mapping

--- src/main/java/kr/ac/jejunu/UserController.java
+++ src/main/java/kr/ac/jejunu/UserController.java
@@ -2,10 +2,7 @@ package kr.ac.jejunu;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,24 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 
 @Controller
-@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     public final UserDao userDao;
 
-    @RequestMapping("/user")
+    @GetMapping("/user")
     public User getUser(@RequestParam("id") Integer id){
+        System.out.println("********** User getUser **********");
         return userDao.findById(id);
     }
 
-    @RequestMapping(path="upload", method = RequestMethod.GET)
+    @GetMapping("upload") // @GetMapping("upload", params="id=1") -> id가 1인 것만 controller 작동
     public void upload(){
 
     }
 
     //업로드한 파일 저장
     //MultipartFileResolver는 binary한 파일을 MultipartFile로 변환시켜 준다
-    @RequestMapping(path="upload", method = RequestMethod.POST)
+    @PostMapping("upload")
     public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
         String path = request.getServletContext().getRealPath("/")
                 + "/WEB-INF/static/" + file.getOriginalFilename();
